2c179a3597de260216ee7a9835c061e38986f171
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-12-17 03:16:35 +0000

    cart_item model 作成

--- work-github/config/routes.rb
+++ work-github/config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :public do
+    resources :items, only: [:show, :index]  
+  end
+
 end
