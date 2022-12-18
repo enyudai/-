5341ea443b3dd03d76216f57afb6706a5264a845
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-12-18 12:07:41 +0000

    admin/items edit記入(レイアウト未調整、動作未確認)

--- work-github/app/controllers/admin/items_controller.rb
+++ work-github/app/controllers/admin/items_controller.rb
@@ -18,9 +18,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
   end
 
   private
