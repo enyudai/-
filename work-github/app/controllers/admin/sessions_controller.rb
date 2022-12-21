b8db920fe78d49b9c860b929ab2754b8488ed686
EC2 Default User <na.maechan@icloud.com>
2022-12-21 06:32:01 +0000

    [Update]管理者から顧客情報編集エラー途中経過

--- work-github/app/controllers/admin/sessions_controller.rb
+++ work-github/app/controllers/admin/sessions_controller.rb
@@ -29,11 +29,17 @@ class Admin::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
-    homes_top_path
+    admin_customers_path
   end
   
   def after_sign_out_path_for(resource)
     admin_session_path
   end
   
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
+  end
 end
