a4af4450ba00093f1e5fe618adb7b9a38067a820
EC2 Default User <na.maechan@icloud.com>
2022-12-21 03:33:01 +0000

    [Update]管理者側の顧客情報一覧編集途中

--- work-github/app/controllers/admin/customers_controller.rb
+++ work-github/app/controllers/admin/customers_controller.rb
@@ -4,7 +4,11 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.all
   end
   
+  def show
+    @customer = Customer.find(params[:id])
+  end
   
-  
-  
+  def edit 
+    
+  end
 end
