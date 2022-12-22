ccb29a39f719c8c30d4b5b019eada03db0774fb4
EC2 Default User <ec2-user@ip-172-31-19-117.ap-northeast-1.compute.internal>
2022-12-22 02:59:32 +0000

    a

--- work-github/db/migrate/20221217022754_create_orders.rb
+++ work-github/db/migrate/20221217022754_create_orders.rb
@@ -2,7 +2,8 @@ class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
       
-      t.integer :payment, null: false
+      t.integer :reception_status, null: false, default: 0
+      t.integer :payment, null: false, default: 0
       t.integer :customer_id, null: false
       t.string :postal_code, null: false
       t.string :address, null: false
