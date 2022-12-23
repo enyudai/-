f9872cba88657a5537dfcd21c47a6ea9cde6bc16
narukawamasa <117150308+narukawamasa@users.noreply.github.com>
2022-12-23 09:34:19 +0000

    マージ前

--- work-github/app/controllers/public/orders_controller.rb
+++ work-github/app/controllers/public/orders_controller.rb
@@ -9,7 +9,7 @@ class Public::OrdersController < ApplicationController
 
     def show
       @order = Order.find(params[:id])
-      @order_detail = OrderDetail.find(params[:id])
+      @order_details = @order.order_details
     end
 
      def new
