1446a5e44e81a24aa69a85750afd54befe50f4cd
EC2 Default User <ec2-user@ip-172-31-19-117.ap-northeast-1.compute.internal>
2022-12-23 10:20:13 +0000

    [update]

--- work-github/db/schema.rb
+++ work-github/db/schema.rb
@@ -108,15 +108,16 @@ ActiveRecord::Schema.define(version: 2022_12_19_044439) do
   create_table "order_details", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "order_detail_id", null: false
     t.integer "order_id", null: false
     t.integer "item_id", null: false
     t.integer "price", null: false
     t.integer "volume", null: false
-    t.integer "production_status", null: false
+    t.integer "production_status", default: 0, null: false
   end
 
   create_table "orders", force: :cascade do |t|
+    t.integer "reception_status", default: 0, null: false
+    t.integer "payment", default: 0, null: false
     t.integer "customer_id", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
