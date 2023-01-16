81b2817fe9c8ed720eadc33d0a4b8128d0c9b8d5
EC2 Default User <ec2-user@ip-172-31-19-117.ap-northeast-1.compute.internal>
2023-01-16 10:48:31 +0000

    [Update1]

--- PicOgi/app/controllers/admin/homes_controller.rb
+++ PicOgi/app/controllers/admin/homes_controller.rb
@@ -1,4 +1,8 @@
 class Admin::HomesController < ApplicationController
+  #adminコントローラーを作成するた度、以下の記述
+  skip_before_action :authenticate_user!
+  before_action :authenticate_admin!
+  
   def top
   end
 end
