35816b4c1f34787446d6e5744d9e7f91f6520cbe
EC2 Default User <ec2-user@ip-172-31-40-25.ap-northeast-1.compute.internal>
2023-01-25 11:17:11 +0000

    [Update]

--- PicOgi/app/controllers/public/homes_controller.rb
+++ PicOgi/app/controllers/public/homes_controller.rb
@@ -1,8 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
-     @answers = Answer.page(params[:page])
+    # @answers = Answer.page(params[:page])
     # @all_ranks = Answer.find(Favorite.group(:answer_id).order('count(answer_id) desc').limit(10).pluck(:answer_id))
     @all_ranks = Answer.where(id: Favorite.group(:answer_id).order('count(answer_id) desc').limit(10).pluck(:answer_id)).page(params[:page]).per(6)
+    
   end
 
   def about
